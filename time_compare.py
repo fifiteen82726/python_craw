5de70e84f159c02892a81102450c4ac2baf2a0fb
fifiteen82726 <fifiteen82726@gmail.com>
2015-06-09 01:25:20 +0800

    add time_compare.py 可以比對資料庫拉出來的時間 and json抓下來的時間

--- time_compare.py
+++ time_compare.py
@@ -3,6 +3,10 @@
 # import the MySQLdb and sys modules
 import MySQLdb
 import sys
+
+import json
+import urllib
+
 # open a database connection
 # be sure to change the host IP address, username, password and database name to match your own
 connection = MySQLdb.connect (host = "140.138.77.104", user = "BDSTeam08", passwd = "BDSTeam08@2015", db = "BDSTeam08_DB")
@@ -17,12 +21,34 @@ cursor.execute("SELECT PublishTime FROM Railfall WHERE SiteId = %s", ("A1AB80",)
 data = cursor.fetchall ()
 
 for row in data :
-	print row[0]
+	select_time = row[0]
 # close the cursor object
 cursor.close ()
 # close the connection
 connection.close ()
-# exit the program
-sys.exit()
+
+
+results = json.load(urllib.urlopen("http://opendata.epa.gov.tw/ws/Data/RainTenMin/?$orderby=PublishTime%20desc&$skip=0&$top=1000&format=json"))
+
+
+results_time = results[0]['PublishTime'] 
+
+print len(results_time)
+print len(select_time)
+
+if  (results_time == select_time):
+	print "tttt"
+
+
+
+
+
+
+
+
+
+
+
+
 
 
