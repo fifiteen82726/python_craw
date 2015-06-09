a6ccdb428ac7daed19525e7ea8b2b48e470064a2
fifiteen82726 <fifiteen82726@gmail.com>
2015-06-10 01:26:36 +0800

    add update_new_json.py 可以update new json file

--- update_data.py
+++ update_data.py
@@ -11,22 +11,17 @@ db = MySQLdb.connect("140.138.77.104","BDSTeam08","BDSTeam08@2015","BDSTeam08_DB
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
-
-# sql =  "INSERT INTO pet_1001514(pet_name, \
-#        pet_owner, pet_specise, pet_sex, pet_birth) \
-#        VALUES ('%s', '%s', '%s', '%s', '%s' )"  % \
-#         ( Mac ,  Mac1 ,  Mac2 ,  Mac3 ,  '0000-00-00' ) 
 try : 
    #執行sql語句
    # cursor . execute ( sql ) 
    cursor.execute ("""
-   UPDATE pet_1001514
-   SET pet_name=%s, pet_owner=%s, pet_specise=%s, pet_sex=%s, pet_birth=%s
-   WHERE pet_sex=%s
-""", ("Year", "Month", "Day", "Hour", "0000-00-00", "Sk"))
-   #提交到數據庫執行
+   UPDATE Railfall
+   SET  Rainfall1hr=%s, PublishTime=%s
+   WHERE SiteId=%s
+""", ("Year", "Month","wr0101"))
    db . commit () 
 except : 
+   #提交到數據庫執行
    #發生錯誤時回滾
    db . rollback ()
 
---
+++ update_new_json.py
@@ -0,0 +1,48 @@
+# -*- coding: UTF-8 -*-     
+#!/usr/bin/python
+
+import MySQLdb
+import json
+import urllib
+
+# Open database connection
+db = MySQLdb.connect("140.138.77.104","BDSTeam08","BDSTeam08@2015","BDSTeam08_DB" )
+
+# prepare a cursor object using cursor() method
+
+
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+
+results = json.load(urllib.urlopen("http://opendata.epa.gov.tw/ws/Data/RainTenMin/?$orderby=PublishTime%20desc&$skip=0&$top=1000&format=json"))
+# sql =  "INSERT INTO pet_1001514(pet_name, \
+#        pet_owner, pet_specise, pet_sex, pet_birth) \
+#        VALUES ('%s', '%s', '%s', '%s', '%s' )"  % \
+#         ( Mac ,  Mac1 ,  Mac2 ,  Mac3 ,  '0000-00-00' ) 
+#print results
+
+try : 
+   #執行sql語句
+   # cursor . execute ( sql )
+   for i in range(0, len(results) , 1): 
+      cursor.execute ("""
+      UPDATE Railfall
+      SET  Rainfall1hr=%s, PublishTime=%s
+      WHERE SiteId=%s
+   """, (results[i]['Rainfall1hr'] ,  results[i]['PublishTime'] , results[i]['SiteId'] ))
+   db . commit () 
+      #print i
+except : 
+   #提交到數據庫執行
+   #發生錯誤時回滾
+   print '7'
+   db . rollback ()
+
+#關閉數據庫連接
+db . close ()
+
+
+
+
